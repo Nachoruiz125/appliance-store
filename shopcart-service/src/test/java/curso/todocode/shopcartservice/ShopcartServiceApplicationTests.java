975ec2f23f5f26ce34d89904c5d57b4d7aece5d6
ignacioruiz <ignaruvi@gmail.com>
2026-01-15 16:55:58 -0300

    Added new Shopcart Service

---
+++ shopcart-service/src/test/java/curso/todocode/shopcartservice/ShopcartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.todocode.shopcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
