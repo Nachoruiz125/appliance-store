8a3e62e8bae5a581da02ff98fe8b8b0eea004585
ignacioruiz <ignaruvi@gmail.com>
2026-01-20 17:16:38 -0300

    Added Sales service folder

---
+++ sales-service/src/main/java/curso/todocode/salesservice/SalesServiceApplication.java
@@ -0,0 +1,13 @@
+package curso.todocode.salesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesServiceApplication.class, args);
+    }
+
+}
---
+++ sales-service/src/test/java/curso/todocode/salesservice/VentasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.todocode.salesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
